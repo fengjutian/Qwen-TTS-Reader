e5744219d69e9e7d038145ca60836ef703e09a7b
fengjutian <2640111081@qq.com>
2025-07-03 23:14:33 +0800

    feat: 新加音频

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
-import { Button } from "./components/ui/button";
-import { Textarea } from "./components/ui/textarea";
+import { AntDesignOutlined } from '@ant-design/icons';
+
+import { Input, Button, ConfigProvider, Space } from 'antd';
+import { createStyles } from 'antd-style';
 
 import React from 'react';
-import styles from './app.module.css';
-import { Input } from 'antd';
+import stylesModule from './app.module.css';
 
 const { TextArea } = Input;
 
-  const baseRoles = [
+const useStyle = createStyles(({ prefixCls, css }) => ({
+  linearGradientButton: css`
+    &.${prefixCls}-btn-primary:not([disabled]):not(.${prefixCls}-btn-dangerous) {
+      > span {
+        position: relative;
+      }
+
+      &::before {
+        content: '';
+        background: linear-gradient(135deg, #6253e1, #04befe);
+        position: absolute;
+        inset: -1px;
+        opacity: 1;
+        transition: all 0.3s;
+        border-radius: inherit;
+      }
+
+      &:hover::before {
+        opacity: 0;
+      }
+    }
+  `,
+}));
+
+
+function App() {
+
+  const { styles } = useStyle();
+
+  const [baseRoles] = useState([
     {
       name: 'Chelsie',
       gender: '女',
@@ -20,40 +48,37 @@ const { TextArea } = Input;
     {
       name: 'Cherry',
       gender: '女',
-      audio: ''
+      audio: '/audio/Cherry_ZH.wav'
     },
     {
       name: 'Ethan',
-      gender: '',
-      audio: ''
+      gender: '男',
+      audio: '/audio/Ethan_ZH.wav'
     },
     {
       name: 'Serena',
       gender: '女',
-      audio: ''
+      audio: '/audio/Serena_ZH.wav'
     }
-  ];
+  ]);
 
-  const newBaseRoles = [
+  const [newBaseRoles] = useState([
     {
-      name: 'Chelsie',
-      gender: '女',
-      audio: '/audio/Chelsie_ZH.wav'
+      name: 'Dylan',
+      gender: '北京话-男',
+      audio: '/audio/北京话-zh.wav'
     },
     {
-      name: 'Chelsie',
-      gender: '女',
-      audio: '/audio/Chelsie_ZH.wav'
+      name: 'Jada',
+      gender: '吴语-女',
+      audio: '/audio/上海话-zh.wav'
     },
         {
-      name: 'Chelsie',
-      gender: '女',
-      audio: '/audio/Chelsie_ZH.wav'
+      name: 'Sunny',
+      gender: '四川话-女',
+      audio: '/audio/四川话-zh.wav'
     },
-  ]
-
-
-function App() {
+  ]);
 
 
   return (
@@ -61,10 +86,10 @@ function App() {
       <h1>通义千问的语音合成模型</h1>
       <h3>Qwen-TTS 是通义千问系列的语音合成模型，支持输入中文、英文、中英混合的文本，并流式输出音频。</h3>
       <h1>支持的音色</h1>
-      <div className={styles['role-list']} >
+      <div className={stylesModule['role-list']} >
         {
           baseRoles.map((role, index) => (
-            <div key={index} className={styles['role-item']}>
+            <div key={index} className={stylesModule['role-item']}>
               <div>{role.name} ({role.gender})</div>
               <audio src={role.audio} controls/>
             </div>
@@ -73,26 +98,28 @@ function App() {
       </div>
 
       <p>qwen-tts-2025-05-22 与 qwen-tts-latest 还支持以下三种音色：</p>
-      <div className={styles['role-list']} >
+      <div className={stylesModule['role-list']} >
         {
           newBaseRoles.map((role, index) => (
-            <div key={index} className={styles['role-item']}>
+            <div key={index} className={stylesModule['role-item']}>
               <div>{role.name} ({role.gender})</div>
               <audio src={role.audio} controls/>
             </div>
           ))
         }
       </div>
-
-
-
-
-
-      <TextArea rows={4} />
-      <br />
-      <br />
-      <TextArea rows={4} placeholder="maxLength is 6" maxLength={6} />
-
+      <TextArea rows={10} placeholder="请输入。最大10000字符" 
+        maxLength={10000} 
+        className={stylesModule['margin-bottom']}/>
+      <ConfigProvider
+        button={{
+          className: styles.linearGradientButton,
+        }}
+        >
+        <Button type="primary" size="large" icon={<AntDesignOutlined />}>
+          生成音频
+        </Button>
+      </ConfigProvider>
     </main>
   );
 }
